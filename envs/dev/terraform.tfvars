8ff1578076d9e8e274c1a97f8d71f0f7da736ebf
OnyiGlobal2025 <onyedikaokoro8@gmail.com>
2026-01-22 23:35:32 +0100

    feat: add VPC module

--- envs/dev/terraform.tfvars
+++ envs/dev/terraform.tfvars
@@ -1,1 +1,4 @@
 aws_region = "us-east-1"
+
+vpc_cidr = "10.0.0.0/16"
+vpc_name = "project3-dev-vpc"
--- envs/dev/variables.tf
+++ envs/dev/variables.tf
@@ -2,3 +2,13 @@ variable "aws_region" {
   description = "AWS region to deploy resources"
   type        = string
 }
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "vpc_name" {
+  description = "Name of the VPC"
+  type        = string
+}
