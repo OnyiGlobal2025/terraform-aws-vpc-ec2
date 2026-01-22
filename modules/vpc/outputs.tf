db3a4af2b83042277950f9ee04ef13712cb4e4a9
OnyiGlobal2025 <onyedikaokoro8@gmail.com>
2026-01-23 00:39:22 +0100

    feat: add internet gateway and route tables

---
+++ modules/vpc/outputs.tf
@@ -0,0 +1,21 @@
+output "vpc_id" {
+  description = "VPC ID"
+  value       = aws_vpc.this.id
+}
+
+output "public_subnet_ids" {
+  value = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
+  value = aws_subnet.private[*].id
+}
+
+output "public_route_table_id" {
+  value = aws_route_table.public.id
+}
+
+output "private_route_table_id" {
+  value = aws_route_table.private.id
+}
+
